28e0331075cb0e81f9530c76ed980fbc976abb3c
zapbuild-ankit <fbg.ankit@gmail.com>
2020-05-19 18:44:37 +0530

    applied queries as in ci

--- application/models/Feature_model.php
+++ application/models/Feature_model.php
@@ -2,36 +2,53 @@
 
 class Feature_model extends CI_Model {
 
-	function savefeatures($feature, $data) {
+	function savefeatures($featuredata) {
 
-		$query = "insert into features values('','$feature','$data')";
-		$this->db->query($query);
+		//$query = "insert into features values('','$feature','$data')";
+		//$this->db->query($query);
+
+		$this->db->insert('features', $featuredata);
 
 	}
 
 	/*View*/
 	function display_records() {
-		$query = $this->db->query("select * from features");
-		return $query->result();
-	}
 
-	function fetch_feature() {
-		$query = $this->db->get("featureselect");
+		//$query = $this->db->query("select * from features");
+
+		$query = $this->db->get("features");
+
 		return $query->result();
+
 	}
 
 	function deleterecords($id) {
-		$this->db->query("delete  from features where id='".$id."'");
+
+		//$this->db->query("delete  from features where id='".$id."'");
+		
+		$this->db->where('id', $id);
+		$this->db->delete('features');
 	}
 
 	function displayrecordsById($id) {
-		$query = $this->db->query("select * from features  where id='".$id."'");
+
+		//$query = $this->db->query("select * from features  where id='".$id."'");
+
+		$this->db->where('id', $id);
+
+		//$this->db->order_by('id', 'desc');
+
+		$query = $this->db->get("features");
+
 		return $query->result();
 	}
 
-	function update_records($feature, $data, $id) {
+	function update_records($featuredata, $id) {
 
-		$query = $this->db->query("update features SET feature='$feature',package='$data' where id='$id'");
+		//$query = $this->db->query("update features SET feature='$feature',package='$data' where id='$id'");
+
+		$this->db->where('id', $id);
+		$this->db->update('features', $featuredata);
 	}
 
 }
