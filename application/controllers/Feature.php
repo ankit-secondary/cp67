70b6e25089178461a6dc82802f199bad28b9ae7f
zapbuild-ankit <fbg.ankit@gmail.com>
2020-05-19 18:43:32 +0530

    modified codes for flash msg and query

--- application/controllers/Feature.php
+++ application/controllers/Feature.php
@@ -13,9 +13,13 @@ class Feature extends CI_Controller {
 
 		$this->load->database();
 
-		// load model crud_model
+		// load model Feature_model
 
 		$this->load->model('Feature_model');
+
+		$this->load->library("session");
+
+		$this->load->library('form_validation');
 
 		$this->load->helper('url');
 
@@ -23,51 +27,87 @@ class Feature extends CI_Controller {
 
 	public function index() {
 		$result['data'] = $this->Feature_model->display_records();
+
 		$this->load->view('/Template/featurelist', $result);
 	}
 
 	public function addfeature() {
-		//  $data['feature'] = $this->Feature_model->fetch_feature();
+
+		$this->load->view('/header');
+
+		$this->form_validation->set_rules('package[]', 'Package', 'required');
+
+		if ($this->form_validation->run() == TRUE) {
+
+			if ($this->input->post('save')) {
+
+				$feature = $this->input->post('feature');
+
+				$package = $this->input->post('package[]');
+
+				$data = implode(',', $package);
+
+				$featuredata = ['feature' => $feature, 'package' => $data];
+
+				$this->Feature_model->savefeatures($featuredata);
+
+				// set flash data to show success message
+
+				$this->session->set_flashdata('success', "<div class='alert alert-success'>successfully added new data</div>");
+
+				return redirect('/index.php/feature/index');
+			} else {
+
+				$this->session->set_flashdata('error', 'please try again');
+			}
+
+		}
 
 		$this->load->view('/Template/insert');
-
-		if ($this->input->post('save')) {
-
-			$feature    = $this->input->post('feature');
-			$package_id = $this->input->post('package');
-			$package    = $this->input->post('package[]');
-
-			$data = implode(',', $package);
-
-			$this->Feature_model->savefeatures($feature, $data);
-			echo 'records saved successfully';
-			return redirect('/codeigniter/index.php/feature/index');
-		}
 
 	}
 
 	public function updatefeature() {
+
+		$this->load->view('/header');
 		$id             = $this->input->get('id');
 		$result['data'] = $this->Feature_model->displayrecordsById($id);
+
+		$this->form_validation->set_rules('package[]', 'Package', 'required');
+
+		if ($this->form_validation->run() == TRUE) {
+
+			if ($this->input->post('update')) {
+				$feature = $this->input->post('feature');
+				$package = $this->input->post('package[]');
+
+				$data = implode(',', $package);
+
+				$featuredata = ['feature' => $feature, 'package' => $data];
+
+				$this->Feature_model->update_records($featuredata, $id);
+
+				$this->session->set_flashdata('success', "<div class='alert alert-success'>successfully updated  data</div>");
+
+				return redirect('/index.php/feature/index');
+			} else {
+				$this->session->set_flashdata('success', "<div class='alert alert-danger'>please try again</div>");
+
+				return redirect('/index.php/feature/index');
+			}
+		}
+
 		$this->load->view('/Template/updatefeatures', $result);
 
-		if ($this->input->post('update')) {
-			$feature = $this->input->post('feature');
-			$package = $this->input->post('package[]');
-
-			$data = implode(',', $package);
-
-			$this->Feature_model->update_records($feature, $data, $id);
-			echo 'data updated successfully!';
-			return redirect('/codeigniter/index.php/feature/index');
-		}
 	}
 
 	public function deletefeature() {
 		$id = $this->input->get('id');
 		$this->Feature_model->deleterecords($id);
-		echo "Date deleted successfully !";
-		return redirect('/codeigniter/index.php/feature/index');
+
+		$this->session->set_flashdata('success', "<div class='alert alert-danger'>successfully deleted  data</div>");
+
+		return redirect('/index.php/feature/index');
 	}
 
 }
