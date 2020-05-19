7b61df54727af50936553b6dd648d959cd282384
zapbuild-ankit <fbg.ankit@gmail.com>
2020-05-19 11:56:30 +0530

    changes

--- application/views/Template/insert.php
+++ application/views/Template/insert.php
@@ -2,14 +2,10 @@
 <head>
 	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <title>Packages having Features </title>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css">
 <h1 class = 'text-center' style='margin-top:100px;'>Add Feature</h1><br>
 <style type="text/css" media="screen">
-.submitform{
-    border:3px solid #f1f1f1;
-    padding: 15px;
-    border-radius: 10px;
 
- }
 </style>
 </head>
 
