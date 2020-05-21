37c74557729d10176a27c65b37e5c2ca75eb808c
zapbuild-ankit <fbg.ankit@gmail.com>
2020-05-21 10:34:27 +0530

    header.php file

--- application/views/header.php
+++ application/views/header.php
@@ -1,9 +1,22 @@
 <html>
         <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
+                <title></title>
 
-                <h1><?php echo $title = 'welcome';?></h1>
-            </body>
-            </html>
+                <!-- jquery ui links -->
+                <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+
+   <script src ='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>
+
+<!-- for client side validation-->
+
+<link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+
+        </head>
